f4c21a9402ad44a32adbc42512cc66d383892eb6
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2024-01-11 17:19:43 +0200

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
     $('.your-class').slick({
         infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        autoplay: true,
+        delay: 10,
+        dots: true
     });
   });
